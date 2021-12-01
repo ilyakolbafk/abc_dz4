8d33bc6cdecec35ea5179dc52a83cab4f0bafee0
ilyakolbafk <ivkolb@edu.hse.ru>
2021-12-01 23:54:07 +0300

    build

---
+++ sort.h
@@ -0,0 +1,10 @@
+//
+// Created by Илья Колб on 01.12.2021.
+//
+
+#ifndef ABC_DZ4_SORT_H
+#define ABC_DZ4_SORT_H
+
+extern void ShakerSort(unsigned char *cont, int len);
+double Average(void *f, int m);
+#endif //ABC_DZ4_SORT_H
